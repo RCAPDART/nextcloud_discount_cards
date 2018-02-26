bb917020afee8cdf8e573b2923bb911efcebe670
Artem.Malozemov <artem.malozemov@valantmed.com>
2018-02-26 13:33:10 +0300

    Optimizing package.json, adding material-ui lib

--- js/App.jsx
+++ js/App.jsx
@@ -1,45 +1,17 @@
 import React, { Component } from 'react';
-import { SideBar, Entry, Separator, App as AppContainer, Content, ControlBar, Settings } from 'oc-react-components';
 import { MainContent } from './components/mainContent/mainContent.jsx';
-
+import './App.less';
 
 export class App extends Component {
-	state = {
-		page: 'home'
-	};
-
-	onClick (page) {
-		this.setState({
-			page: page
-		});
-	}
-
+    state = {
+        page: "Main"
+    };
 	render () {
 		return (
-			<AppContainer appId="react_oc_boilerplate">
-				<SideBar withIcon={true}>
-					<Entry key={'1'} icon="home" onClick={this.onClick.bind(this,'home')}>Favorite cards</Entry>
-					<Entry key={'2'} icon="link" onClick={this.onClick.bind(this,'link')}>All cards</Entry>
-					<Entry key={'3'} icon="folder" onClick={this.onClick.bind(this,'folder')}>Categories</Entry>
-					<Separator/>
-					<Entry key={'4'} icon="user" onClick={this.onClick.bind(this,'user')}>Disabled</Entry>
-
-					<Settings>
-						<h2>
-							Here are all settings!
-						</h2>
-					</Settings>
-				</SideBar>
-
-				<ControlBar>
-					<input type="text" placeholder="Find card"/>
-				</ControlBar>
-
-				<Content key={4}>
-					<h1>{this.state.page}</h1>
-					<MainContent name={this.state.page}/>
-				</Content>
-			</AppContainer>
+			<div className='discountCardsMainContainer'>
+				<h1>{this.state.page}</h1>
+				<MainContent name={this.state.page}/>
+			</div>
 		);
 	}
 }
--- templates/index.php
+++ templates/index.php
@@ -1,1 +1,2 @@
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet">
 <?php script($_['appId'], ['../build/main']); ?>
