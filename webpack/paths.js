06f6d335a3dcd942c6b75bfed69bb4b20fca0feb
Artem.Malozemov <a.malozyomov@smartcat.ai>
2022-04-19 09:35:18 +0300

    Updating paths

--- webpack/paths.js
+++ webpack/paths.js
@@ -3,10 +3,10 @@ const path = require('path');
 module.exports = {
   root: path.resolve(__dirname, '../'),
   outputPath: path.resolve(__dirname, '../', 'build'),
-  entryPath: path.resolve(__dirname, '../', 'js/index.jsx'),
+  entryPath: path.resolve(__dirname, '../', 'src/index.jsx'),
   templatePath: path.resolve(__dirname, '../', 'templates/index.php'),
-  imagesFolder: 'build',
-  fontsFolder: 'build',
-  cssFolder: 'build',
-  jsFolder: 'build'
+  imagesFolder: 'images',
+  fontsFolder: 'fonts',
+  cssFolder: 'css',
+  jsFolder: 'js'
 };
