d295aaff81ffd242a818d30ae70be6bc5625b7a1
Artem.Malozemov <a.malozyomov@smartcat.ai>
2022-04-19 09:44:54 +0300

    Fix paths

--- webpack/paths.js
+++ webpack/paths.js
@@ -5,6 +5,7 @@ module.exports = {
   entryPath: path.resolve(__dirname, '../', 'src/index.jsx'),
   imagesFolder: path.resolve(__dirname, '../', 'img'),
   fontsFolder: path.resolve(__dirname, '../', 'fonts'),
-  cssFolder: path.resolve(__dirname, '../', 'css'),
+  cssFolder: '../css',
+  cssFolder2: path.resolve(__dirname, '../', 'css'),
   jsFolder: path.resolve(__dirname, '../', 'js')
 };
